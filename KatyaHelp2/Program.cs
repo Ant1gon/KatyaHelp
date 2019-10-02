0d0d1c66f2f406de92bed0c57854e6db2bece718
Ant1gon <sergkropotov@ukr.net>
2019-10-03 00:50:23 +0300

    Need MORE fixes!!!!! added time validate change time format (add seconds) added work with unknown commands change date validate regex

--- KatyaHelp2/Program.cs
+++ KatyaHelp2/Program.cs
@@ -13,178 +13,212 @@ namespace KatyaHelp2
 	{
 		static void Main(string[] args)
 		{
-			if (args.Any())
+			try
 			{
-				string date = "", tender = "";
-				string _SEPARATOR = "|";
-				string command_SEPARATOR = ";";
-				string dateValidator = "(19|20)\\d\\d-((0[1-9]|1[012])-(0[1-9]|[12]\\d)|(0[13-9]|1[012])-30|(0[13578]|1[02])-31)";
-
-				List<Dictionary<string, string>> listDictForFile = new List<Dictionary<string, string>>();
-				//string[] commandList = ConfigurationManager.AppSettings.Get("commandList").Trim().Split(command_SEPARATOR, StringSplitOptions.RemoveEmptyEntries);
-
-				StreamReader sr = new StreamReader(args[0]);
-				string line;
-				List<string> lineList = new List<string>();
-				while (!sr.EndOfStream)
+				if (args.Any())
 				{
-					line = sr.ReadLine().Trim();
-					if (line.Length > 1)
+					string date = "", tender = "";
+					string _SEPARATOR = "|";
+					string _COMMAND_SEPARATOR = ";";
+					string dateValidator = "(19|20)\\d\\d-((0[1-9]|1[012])-(0[1-9]|[12]\\d)|(0[13-9]|1[012])-30|(0[13578]|1[02])-31)";
+					dateValidator = "^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d$";
+					string[] activeCommands = ConfigurationManager.AppSettings.Get("commandList").Trim().Split(new string[] { _COMMAND_SEPARATOR }, StringSplitOptions.RemoveEmptyEntries);
+
+					List<Dictionary<string, string>> listDictForFile = new List<Dictionary<string, string>>();
+					//string[] commandList = ConfigurationManager.AppSettings.Get("commandList").Trim().Split(command_SEPARATOR, StringSplitOptions.RemoveEmptyEntries);
+
+					string testFile = string.Format("{0}\\{1}", Environment.CurrentDirectory, "test.txt");
+					//StreamReader sr = new StreamReader(testFile);
+
+					StreamReader sr = new StreamReader(args[0]);
+					string line;
+					List<string> lineList = new List<string>();
+					while (!sr.EndOfStream)
 					{
-						lineList.Add(line);
-					}
-				}
-				sr.Close();
-				//foreach (var t in lineList)
-				for (int i = 0; i < lineList.Count; i++)
-				{
-					string time = "", ip = "", command = "", commandName = "", proposition = "";
-					Dictionary<string, string> dictForFile = new Dictionary<string, string>();
-					var t = lineList[i];
-					string[] temp = t.Split(new string[] { _SEPARATOR }, StringSplitOptions.RemoveEmptyEntries);
-					if (temp.Length == 1)
-					{
-						Match match = Regex.Match(temp[0], dateValidator);
-						if (match.Success)
+						line = sr.ReadLine().Trim();
+						if (line.Length > 1)
 						{
-							DateTime tDate;
-							DateTime.TryParse(temp[0].Trim(), out tDate);
-							date = tDate.ToLongDateString();
+							lineList.Add(line);
 						}
 					}
-					else
+					sr.Close();
+					//foreach (var t in lineList)
+					for (int i = 0; i < lineList.Count; i++)
 					{
-						time = DateTime.ParseExact(temp[0].Remove(temp[0].IndexOf(" ->")).Remove(temp[0].IndexOf(".")).Replace(":", ""),
-								"HHmmss", System.Globalization.CultureInfo.InvariantCulture).ToString("HH:mm");
-						if (temp[1].Trim().Equals("In"))
+						string ip = "", command = "", commandName = "", proposition = "";
+						DateTime time = DateTime.Now;
+						Dictionary<string, string> dictForFile = new Dictionary<string, string>();
+						var t = lineList[i];
+						string[] temp = t.Split(new string[] { _SEPARATOR }, StringSplitOptions.RemoveEmptyEntries);
+						if (temp.Length == 1)
 						{
-							ip = temp[2].Trim().Replace("IP:", "").Trim();
-							command = temp[3].Trim().Replace("CommandName:", "").Trim();
-							commandName = ConfigurationManager.AppSettings.Get(command).Trim();
-
-							if (command.Equals("Upload"))
+							if (Regex.Match(temp[0], dateValidator).Success)
 							{
-								List<string> filesName = new List<string>();
-								for (int ii = 1; ii < lineList.Count; ii++)
+								DateTime tDate;
+								if (DateTime.TryParse(temp[0].Trim(), out tDate))
 								{
-									try
-									{
-										var temp2 = lineList[i + ii].Split(new string[] { _SEPARATOR }, StringSplitOptions.RemoveEmptyEntries);
-										if (temp2[1].Trim().Equals("Out"))
-										{
-											ii = lineList.Count;
-										}
-										else if (temp2[1].Trim().Equals("info"))
-										{
-											filesName.Add(temp2[2]);
-										}
-									}
-									catch { };
+									date = tDate.ToLongDateString();
 								}
-								string fN = string.Join(",", filesName);
-								commandName = string.Format(ConfigurationManager.AppSettings.Get("Upload").Trim(), tender, fN);
 							}
 						}
-						else if (temp[1].Trim().Equals("page"))
+						else
 						{
-							ip = temp[3].Trim().Replace("IP: ", "");
-							if (temp[2].Trim().StartsWith("/PositionForm"))
+							var u = Regex.Match(temp[0], "(\\d{1}|\\d{2})\\:(\\d{1}|\\d{2})\\:(\\d{2}|\\d{1})");
+							if (u.Success)
 							{
-								tender = temp[2].Trim().Split(new string[] { "=" }, StringSplitOptions.RemoveEmptyEntries)[1];
-								commandName = string.Format(ConfigurationManager.AppSettings.Get("/PositionForm").Trim(), tender);
-
-							}
-							else if (temp[2].Trim().StartsWith("/BidForm"))
-							{
-								proposition = temp[2].Trim().Split(new string[] { "=" }, StringSplitOptions.RemoveEmptyEntries)[1];
-								commandName = string.Format(ConfigurationManager.AppSettings.Get("/BidForm").Trim(), tender);
+								DateTime.TryParse(u.Value, out time);
 							}
 
+							//string ttime = temp[0].Substring(temp[0].IndexOf(": ")).Remove(temp[0].IndexOf(" ->")).Remove(temp[0].IndexOf(".")).Replace(":", "");
+							/*time = DateTime.ParseExact(
+								temp[0].Substring(temp[0].IndexOf(": "))
+									   .Remove(temp[0].IndexOf(" ->"))
+									   .Remove(temp[0].IndexOf("."))
+									   .Replace(":", ""),
+									"HHmmss", System.Globalization.CultureInfo.InvariantCulture
+									).ToString("HH:mm");*/
+							if (temp[1].Trim().Equals("In"))
+							{
+								ip = temp[2].Trim().Replace("IP:", "").Trim();
+								command = temp[3].Trim().Replace("CommandName:", "").Trim();
+								if(activeCommands.Any(y => y.Equals(command))){ 
+									commandName = ConfigurationManager.AppSettings.Get(command).Trim(); ////
+									if (command.Equals("Upload"))
+									{
+										List<string> filesName = new List<string>();
+										for (int ii = 1; ii < lineList.Count; ii++)
+										{
+											try
+											{
+												var temp2 = lineList[i + ii].Split(new string[] { _SEPARATOR }, StringSplitOptions.RemoveEmptyEntries);
+												if (temp2[1].Trim().Equals("Out"))
+												{
+													ii = lineList.Count;
+												}
+												else if (temp2[1].Trim().Equals("info"))
+												{
+													filesName.Add(temp2[2]);
+												}
+											}
+											catch { };
+										}
+										string fN = string.Join(",", filesName);
+										commandName = string.Format(ConfigurationManager.AppSettings.Get("Upload").Trim(), tender, fN);
+									}
+								}
+								else
+								{
+									commandName = ConfigurationManager.AppSettings.Get("UnknownCommand").Trim();
+								}
+
+								
+							}
+							else if (temp[1].Trim().Equals("page"))
+							{
+								ip = temp[3].Trim().Replace("IP: ", "");
+								if (temp[2].Trim().StartsWith("/PositionForm"))
+								{
+									tender = temp[2].Trim().Split(new string[] { "=" }, StringSplitOptions.RemoveEmptyEntries)[1];
+									commandName = string.Format(ConfigurationManager.AppSettings.Get("/PositionForm").Trim(), tender);
+
+								}
+								else if (temp[2].Trim().StartsWith("/BidForm"))
+								{
+									proposition = temp[2].Trim().Split(new string[] { "=" }, StringSplitOptions.RemoveEmptyEntries)[1];
+									commandName = string.Format(ConfigurationManager.AppSettings.Get("/BidForm").Trim(), tender);
+								}
+
+							}
+						}
+						//Console.ReadKey();
+						dictForFile.Add("date", date);
+						dictForFile.Add("time", time.ToString("HH:mm:ss"));
+						dictForFile.Add("ip", ip);
+						dictForFile.Add("commandName", commandName);
+						//string h = dictForFile["date"];
+						if (!string.IsNullOrEmpty(dictForFile["commandName"]))
+						{
+							listDictForFile.Add(dictForFile);
 						}
 					}
-					//Console.ReadKey();
-					dictForFile.Add("date", date);
-					dictForFile.Add("time", time);
-					dictForFile.Add("ip", ip);
-					dictForFile.Add("commandName", commandName);
-					//string h = dictForFile["date"];
-					if (!string.IsNullOrEmpty(dictForFile["commandName"]))
-					{
-						listDictForFile.Add(dictForFile);
-					}
-				}
 
-				#region xlsFileCreate
-				Random a = new Random();
-				string correctionsFile = string.Format("{0}\\{1}.xlsx", Environment.CurrentDirectory, a.Next(0, 4578));
-				if (File.Exists(correctionsFile))
-				{
-					File.Delete(correctionsFile);
-				}
+					#region xlsFileCreate
+					Random a = new Random();
+					string correctionsFile = string.Format("{0}\\{1}.xlsx", Environment.CurrentDirectory, a.Next(0, 4578));
+					if (File.Exists(correctionsFile))
+					{
+						File.Delete(correctionsFile);
+					}
 
-				var file = new FileInfo(correctionsFile);
-				var package = new ExcelPackage(file);
-				ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Work");
+					var file = new FileInfo(correctionsFile);
+					var package = new ExcelPackage(file);
+					ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Work");
 
-				// --------- Data and styling goes here -------------- //
-				int col = 1, row = 1;
-				worksheet.DefaultColWidth = 25;
-				worksheet.Cells[row, col++].Value = ConfigurationManager.AppSettings.Get("firstColumn").Trim();
-				worksheet.Cells[row, col++].Value = ConfigurationManager.AppSettings.Get("secondColumn").Trim();
-				worksheet.Cells[row, col++].Value = ConfigurationManager.AppSettings.Get("thirdColumn").Trim();
-				worksheet.Cells[row, col++].Value = ConfigurationManager.AppSettings.Get("fourthColumn").Trim();
-				int iii = 2;
-				foreach (var entity in listDictForFile)
-				{
-					col = 1;
-					try
+					// --------- Data and styling goes here -------------- //
+					int col = 1, row = 1;
+					worksheet.DefaultColWidth = 25;
+					worksheet.Cells[row, col++].Value = ConfigurationManager.AppSettings.Get("firstColumn").Trim();
+					worksheet.Cells[row, col++].Value = ConfigurationManager.AppSettings.Get("secondColumn").Trim();
+					worksheet.Cells[row, col++].Value = ConfigurationManager.AppSettings.Get("thirdColumn").Trim();
+					worksheet.Cells[row, col++].Value = ConfigurationManager.AppSettings.Get("fourthColumn").Trim();
+					int iii = 2;
+					foreach (var entity in listDictForFile)
 					{
-						worksheet.Cells[iii, col++].Value = entity["ip"];
+						col = 1;
+						try
+						{
+							worksheet.Cells[iii, col++].Value = entity["ip"];
+						}
+						catch { }
+						try
+						{
+							worksheet.Cells[iii, col++].Value = entity["date"];
+							//worksheet.Cells[i, 2].Value = entity["rgc_value"].ToString().Remove(entity["rgc_value"].ToString().IndexOf(",") + 2);
+						}
+						catch { }
+						try
+						{
+							worksheet.Cells[iii, col++].Value = entity["time"];
+						}
+						catch { }
+						try
+						{
+							worksheet.Cells[iii, col++].Value = entity["commandName"];
+						}
+						catch { }
+						iii++;
 					}
-					catch { }
-					try
+					var startRow = 2;
+					var startColumn = 1;
+					var endRow = 100;
+					var endColumn = 10;
+					///my
+					int[] sortColumn = new int[] { 1, 2 };
+					bool[] vs = new bool[] { false, false };
+					///my
+					using (ExcelRange excelRange = worksheet.Cells[startRow, startColumn, endRow, endColumn])
 					{
-						worksheet.Cells[iii, col++].Value = entity["date"];
-						//worksheet.Cells[i, 2].Value = entity["rgc_value"].ToString().Remove(entity["rgc_value"].ToString().IndexOf(",") + 2);
+						excelRange.Sort(sortColumn, vs, null, CompareOptions.None);
 					}
-					catch { }
-					try
-					{
-						worksheet.Cells[iii, col++].Value = entity["time"];
-					}
-					catch { }
-					try
-					{
-						worksheet.Cells[iii, col++].Value = entity["commandName"];
-					}
-					catch { }
-					iii++;
-				}
-				var startRow = 2;
-				var startColumn = 1;
-				var endRow = 100;
-				var endColumn = 10;
-				///my
-				int[] sortColumn = new int[] { 1, 2 };
-				bool[] vs = new bool[] { false, false };
-				///my
-				using (ExcelRange excelRange = worksheet.Cells[startRow, startColumn, endRow, endColumn])
-				{
-					excelRange.Sort(sortColumn, vs, null, CompareOptions.None);
-				}
-				package.Save();
+					package.Save();
 
-				/*if (File.Exists(correctionsFile))
+					/*if (File.Exists(correctionsFile))
+					{
+						File.Delete(correctionsFile);
+					}*/
+					#endregion xlsFileCreate
+					Console.WriteLine(string.Format("End{0}Press any key to continue", Environment.NewLine));
+					Console.ReadKey();
+				}
+				else
 				{
-					File.Delete(correctionsFile);
-				}*/
-				#endregion xlsFileCreate
-				Console.WriteLine(string.Format("End{0}Press any key to continue", Environment.NewLine));
-				Console.ReadKey();
+					Console.WriteLine(ConfigurationManager.AppSettings.Get("noFile").Trim());
+					Console.ReadKey();
+				}
 			}
-			else
+			catch (Exception e)
 			{
-				Console.WriteLine(ConfigurationManager.AppSettings.Get("noFile").Trim());
+				Console.WriteLine(e);
 				Console.ReadKey();
 			}
 		}
