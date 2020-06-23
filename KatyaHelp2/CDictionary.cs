833fc3af5f4802282bea1a9fc578fff50dbb0e23
Serhii <sergkropotov@ukr.net>
2020-06-23 18:33:11 +0300

    up

---
+++ KatyaHelp2/CDictionary.cs
@@ -0,0 +1,6 @@
+﻿namespace KatyaHelp2
+{
+    internal class CommandDictionary<T1, T2, T3, T4>
+    {
+    }
+}
---
+++ KatyaHelp2/Dictionary.cs
@@ -0,0 +1,9 @@
+﻿namespace KatyaHelp2
+{
+    internal class Dictionary<T1, T2, T3, T4>
+    {
+        public Dictionary()
+        {
+        }
+    }
+}
